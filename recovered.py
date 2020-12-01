bd46f6139afb81a25cfc5b1381ef2b22225763a4
Maya Chalker <mayachalker@Mayas-MBP-2.fios-router.home>
2020-12-01 18:40:56 -0500

    Work on 25 at a time

--- recovered.py
+++ recovered.py
@@ -10,6 +10,24 @@ def setUpDatabase(db_name):
     cur = conn.cursor()
     return cur, conn
 
+def create_month_table(cur, conn):
+    cur.execute('CREATE TABLE Months (key TEXT PRIMARY KEY, month_name TEXT)')
+    i = 1
+    month_lst = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    while i <= 12:
+        for month in month_lst:
+            if len(str(i)) == 1:
+                new = '0' + str(i)
+            else:
+                new = str(i)
+            cur.execute("INSERT INTO Months (key, month_name) VALUES (?, ?)", (new, month))
+            i += 1
+    conn.commit()
+
+def create_recovered_table(cur, conn):
+    cur.execute('CREATE TABLE IF NOT EXISTS Recovered (key TEXT PRIMARY KEY, date TEXT UNIQUE, month TEXT, new_recovered INTEGER, total_recovered INTEGER)')
+    conn.commit()
+
 def create_request_url(state, start_date, end_date):
     ''' Date should be in the format YYYY-MM-DD '''
     base_url = "https://api.covid19tracking.narrativa.com/api/country/us"
@@ -17,7 +35,6 @@ def create_request_url(state, start_date, end_date):
     return url
 
 def get_data():
-    cur.execute('CREATE TABLE IF NOT EXISTS Recovered (key TEXT PRIMARY KEY, date TEXT UNIQUE, new_recovered INTEGER, total_recovered INTEGER)')
     current_month = 5
     end_month = current_month + 1
     d = {}
@@ -29,7 +46,7 @@ def get_data():
         new_end_month = str(end_month)
         if len(new_end_month) == 1:
             new_end_month = '0' + new_end_month
-        request_url = create_request_url('michigan', '2020-{}-12'.format(new_month), '2020-{}-12'.format(new_end_month))
+        request_url = create_request_url('michigan', '2020-{}-01'.format(new_month), '2020-{}-01'.format(new_end_month))
         r = requests.get(request_url)
         data = json.loads(r.text)
         for day in data['dates']:
@@ -38,26 +55,32 @@ def get_data():
         current_month += 1
         end_month += 1
     return d
+
     
-    def add_recovered_data(cur, conn, d):
-        counter = 0
-        while counter < len(d['dates']):
-            i = 0
-            while i < 25:
-                for date in d['dates']:
-                    day = date
-                    new_recovered = d['dates'][date]['countries']['US']['today_new_recovered']
-                    total_recovered = d['dates'][date]['countries']['US']['today_recovered']
-                    cur.execute("INSERT INTO Recovered (key, date, new_recovered, total_recovered) VALUES (?, ?, ?, ?)", 
-                    (counter, day, new_recovered, total_recovered))
-                    counter += 1
-                    i += 1
-            conn.commit()
+def add_recovered_data(cur, conn, d):
+    counter = 0
+    while counter < len(d['dates']):
+        i = 0
+        while i < 25:
+            for date in d['dates']:
+                day = date
+                new_recovered = d['dates'][date]['countries']['US']['today_new_recovered']
+                total_recovered = d['dates'][date]['countries']['US']['today_recovered']
+                cur.execute('SELECT month_name FROM Months WHERE key = ?', (day[5:7],))
+                month = cur.fetchone()[0]
+                cur.execute("INSERT INTO Recovered (key, date, month, new_recovered, total_recovered) VALUES (?, ?, ?, ?, ?)", 
+                (counter, day, month, new_recovered, total_recovered))
+                counter += 1
+                i += 1
+        conn.commit()
 
 def main():
     # SETUP DATABASE AND TABLE
     cur, conn = setUpDatabase('covid_tracking.db')
-    add_data(cur, conn)
+    create_month_table(cur, conn)
+    create_recovered_table(cur, conn)
+    dic = get_data()
+    add_recovered_data(cur, conn, dic)
 
 if __name__ == "__main__":
     main()
