6575569134dbb050eaed1ea8da5a0777130af812
enisbet <enisbet@umich.edu>
2020-12-06 21:13:23 -0500

    25 at a time done

--- casesByDate.py
+++ casesByDate.py
@@ -5,17 +5,18 @@ import os
 import sqlite3
 
 def month_table(cur,conn):
-    #creates a new table named months with three columns:
+   #creates a new table named months with three columns:
     # (1) month_id (integer primary key)
     # (2) month_num (text) 
     # (2) month (text) 
     months = ["May", "June", "July", "August", "September", "October"]
     num_months = ["05","06","07", "08","09","10"]
-    cur.execute("CREATE TABLE IF NOT EXISTS months (month_id INTEGER PRIMARY KEY,month_num TEXT, month TEXT)")
+    cur.execute("DROP TABLE IF EXISTS months")
+    cur.execute("CREATE TABLE months (month_id INTEGER PRIMARY KEY,month_num TEXT, month TEXT)")
     for i in range(0,len(months)):
         cur.execute("INSERT INTO months (month_id,month_num,month) VALUES (?,?,?)", (i,num_months[i], months[i],))
     conn.commit()
-
+    
     
 def get_data():
     # tries to request the data from the API if there is an error it prints "error when requesting data from API" 
@@ -66,33 +67,47 @@ def setUpDatabase(db_name):
     return cur, conn
 
 def add_data_to_table(cur,conn):
-    # adds data returned from the clean_data() function to a new table named Cases with four columns: 
-    # (1) month_id (text) HINT: Found from the months table 
-    # (2) date (text in form of 2020-DD-MM)
-    # (3) new_cases (integer)
-    # (4) total_cases (integer)
-    # NOTE: Adds 25 items to the table at a time 
+    # adds data (25 items each time the code is run) returned from the clean_data() function to a new table named Cases with five columns: 
+    # (1) id (integer primary key)
+    # (2) month_id (text) HINT: Found from the months table 
+    # (3) date (text in form of 2020-DD-MM)
+    # (4) new_cases (integer)
+    # (5) total_cases (integer)
     data = clean_data()
-    cur.execute('CREATE TABLE IF NOT EXISTS Cases (month_id INTEGER, date TEXT, new_cases INTEGER, total_cases INTEGER)')
+    cur.execute('CREATE TABLE IF NOT EXISTS Cases (id INTEGER PRIMARY KEY, month_id INTEGER, date TEXT, new_cases INTEGER, total_cases INTEGER)')
+    data2 = data[5:]
+    cur.execute("SELECT id FROM Cases")
+    lst = cur.fetchall()
+
+    # only will happen the first time ran (when len(lst) == 0)
+    start = 0 
+
+    if len(lst) > 0:
+        start = int(lst[-1][0]) + 1
     
-    count = 5  
-    # needed to start count at 5 because I didn't want the first 5 items in the data set
-    while count < len(data):
-        i = 0 
-        while i < 25:
-            if count >= len(data):
-                break
-            else:
-                month_num = data[count][0][5:7]
-                cur.execute("SELECT month_id FROM months WHERE month_num = ?", (month_num,))
-                month_id = cur.fetchone()[0]
-                date = data[count][0]
-                total_cases = data[count][1]
-                new_cases = data[count][2]
-                cur.execute("INSERT INTO Cases (month_id,date,new_cases, total_cases) VALUES (?,?,?,?)",(month_id,date, new_cases,total_cases,))
-                count += 1
-                i += 1
+   
+    end = start + 25 
+
+    i = 0
+    for i in range(start,end):
+         if i >= len(data2):
+             i = i - 1
+             break
+         else:
+            month_num = data2[i][0][5:7]
+            cur.execute("SELECT month_id FROM months WHERE month_num = ?", (month_num,))
+            month_id = cur.fetchone()[0]
+            date = data2[i][0]
+            total_cases = data2[i][1]
+            new_cases = data2[i][2]
+            cur.execute("INSERT INTO Cases (id, month_id,date,new_cases, total_cases) VALUES (?,?,?,?,?)",( i, month_id,date, new_cases,total_cases,))
     conn.commit()
+
+    print("======================================")
+    print("entered more items into database, you now have " + str(i + 1) + " items in the database\n")
+    print("there is a total of " + str(len(data2)) + " items that you are able to put into the db\n")
+    print("please run again to put more items in the db\n")
+    print("======================================")
 
 
 
