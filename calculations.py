c1b56929beddf4804fcb1c9d23f1d73475b74483
Maya Chalker <mayachalker@Mayas-MBP-2.fios-router.home>
2020-12-11 14:35:04 -0500

    calculations and outfile

--- calculations.py
+++ calculations.py
@@ -50,6 +50,53 @@ def graph_cases(data):
     plt.title('Number of COVID-19 Cases by Month in 2020', color = 'purple')
     plt.show()
 
+def calculate_recovered_totals(cur, conn, month, filename):
+    ''' This function calculates the total number of recovered cases for each month. '''
+    total = 0
+    cur.execute('SELECT new_recovered FROM Recovered WHERE month = ?', (month, ) )
+    values = cur.fetchall()
+    for tup in values:
+        num = tup[0]
+        total += num
+    with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), filename), 'w') as f:
+        f.write("Total Recoveries from COVID for {}: {} \n".format(month, total))
+
+
+def recovered_for_graphing(cur, conn):
+    cur.execute('SELECT DISTINCT month FROM Recovered')
+    data = cur.fetchall()
+    months = []
+    for info in data:
+        months.append(info[0])
+    d = {}
+    for month in months[:-1]:
+        cur.execute('SELECT new_recovered FROM Recovered WHERE month = ?', (month, ) )
+        values = cur.fetchall()
+        total = 0
+        for tup in values:
+            num = tup[0]
+            total += num
+        if month not in d:
+            d[month] = total
+    return d
+
+def write_calculations(filename, d, data, dic):
+    with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), filename), 'w') as f:
+        for month in data:
+            f.write("Total COVID Cases for {} 2020: {} \n".format(month, data[month]))
+        for item in dic:
+            f.write("Total Deaths from COVID for {} 2020: {} \n".format(item, dic[item]))
+        for x in d:
+            f.write("Total Recoveries from COVID for {} 2020: {} \n".format(item, d[item]))
+        
+
+def graph_recovered(d):
+    x = d.keys()
+    y = d.values()
+    plt.bar(x, y, color='blue')
+    plt.xlabel('Month Name')
+    plt.ylabel('Number of Recovered Cases')
+    plt.show()
 
 
 
@@ -58,6 +105,9 @@ def main():
     cur,conn = setUpDatabase('covid_tracking.db')
     data = cases_calculations(cur,conn)
     graph_cases(data)
+    d = recovered_for_graphing(cur,conn)
+    write_calculations("calculations_outfile", d)
+    graph_recovered(d)
 
 if __name__ == "__main__":
     main()
